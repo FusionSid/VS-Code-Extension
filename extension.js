9c2d08bdce6ee5bb9f8cae0205f5aea1ff35559a
FusionSid <siddheshadsv@icloud.com>
2022-02-27 17:24:01 +1300

    Added Abandon meme command (https://memegenapi.herokuapp.com/api/abandon/?text=e)

--- extension.js
+++ extension.js
@@ -3,18 +3,33 @@ const vscode = require('vscode');
 /**
  * @param {vscode.ExtensionContext} context
  */
-function activate(context) {
+async function activate(context) {
 	vscode.window.showInformationMessage("Extension Activated");
 	
 	console.log('"imageapi" is now active!');
 
-	let disposable = vscode.commands.registerCommand('imageapi.helloWorld', function () {
+	let disposable = vscode.commands.registerCommand('imageapi.helloWorld', async function () {
 
 		vscode.window.showInformationMessage('Hello World');
+	});
+
+	let abandon = vscode.commands.registerCommand("imageapi.abandon", async function () {
+
+		const searchQuery = await vscode.window.showInputBox({
+			placeHolder: "Enter Text"
+		  });
+		if(searchQuery == ''){
+			return vscode.window.showErrorMessage('You must input text for this to work');
+		}
+		const text = searchQuery.replace(" ", "+")
+		vscode.env.showInformationMessage("Image Saved!");
+		vscode.env.openExternal(`https://memegenapi.herokuapp.com/api/abandon/?text=${text}`);
+
 	});
 	
 
 	context.subscriptions.push(disposable);
+	context.subscriptions.push(abandon);
 }
 
 function deactivate() {}
